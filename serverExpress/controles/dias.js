6a950f8bcec379ce282e09e82beafb69c14935ba
e-cadena <36049644+e-cadena@users.noreply.github.com>
2020-03-11 11:25:17 -0500

    Add files via upload
    cambios 11:24 am

---
+++ serverExpress/controles/dias.js
@@ -0,0 +1,68 @@
+;
+let config = require('../knexfile')
+let env = 'development'
+let db = require('knex')(config[env])
+
+
+
+let leerTablaId = (req, res) => {
+    let campos = req.body.campos
+    let tabla = req.body.tabla
+    let condicion = req.body.condicion
+    db(tabla).where(condicion).select(campos)
+    .then(registros => {
+        return res.status(200).json({
+            datos: registros
+        })
+    })
+    .catch(error => {
+        return res.status(404).json({
+            datos: error
+        })
+    })
+}
+
+let ingresarDia = (req,res) => {
+    let campos = req.body.campos
+    let tabla = req.body.tabla
+    console.log(campos.primerNombre)
+    console.log(tabla)
+    db(tabla).insert(campos)
+    .then(registro => {
+        return res.status(200).json({
+            mensaje: "el registro due guardado"
+        })
+    })
+    .catch(error => {
+        return res.status(404).json({
+            mensaje: error
+        })
+    })
+}
+
+
+let modificarDia = (req, res) => {
+    let campos = req.body.campos
+    let tabla = req.body.tabla
+    let condicion = req.body.condicion
+    db(tabla).where(condicion[0], condicion[1], condicion[2]).update(campos)
+    .then(registro => {
+        return res.status(200).json({
+            mensaje: "El registro fue modificado"
+        })
+        
+    })
+    .catch(error => {
+        return res.status(404).json({
+            mensaje: error
+        })
+    })
+}
+
+
+module.exports = {
+   leerTabla,
+   leerTablaId,
+   ingresarDia,
+   modificarDia
+}
